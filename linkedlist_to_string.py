1ab31f50e7264e2c20b1ce9fbb289ff3e64942cf
MariaOnyshchuk <onyshchuk.pn@ucu.edu.ua>
2024-04-06 11:34:43 +0300

    changes for pylint

--- linkedlist_to_string.py
+++ linkedlist_to_string.py
@@ -1,13 +1,15 @@
 class Node():
+    '''class of Nodes for linked list'''
     def __init__(self, data, next = None):
         self.data = data
         self.next = next
 
 def stringify(node):
+    '''convert linked list to string'''
     lst=''
-    while node!=None:
+    while node is not None:
         lst+=str(node.data)+' -> '
         node = node.next
-        
+
     lst+='None'
     return lst
