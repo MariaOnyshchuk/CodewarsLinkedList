d4474067d81f9359a9e48a33e2aee9d766675ed6
MariaOnyshchuk <onyshchuk.pn@ucu.edu.ua>
2024-04-06 12:07:09 +0300

    we go through string from back to front. if there is only None node, def returns None, in other way it returns linked list

---
+++ string_to_linkedlist.py
@@ -0,0 +1,20 @@
+class Node:
+    '''class of Nodes for linked list'''
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def linked_list_from_string(s):
+    '''convert string to linked list'''
+    lst = s.split(' -> ')[::-1]
+    if lst.pop(0) == 'None':
+        prev_node = None
+    if not lst:
+        return None
+    while lst:
+        curr_node = Node(int(lst.pop(0)), prev_node)
+        prev_node = curr_node
+    return curr_node
+            
+    # return # head to linked list
+print(linked_list_from_string("None"))
